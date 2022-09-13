2733c4b36c05a505eda373e632fba2323eeb19e6
DOUGLAS BASTOS MERENCIO <76476827+Douglasbm040@users.noreply.github.com>
2022-09-13 14:47:07 -0300

    Create CLASS_PESSOA.sql

---
+++ exercism/binary/CLASS_PESSOA.sql
@@ -0,0 +1,50 @@
+create or replace package PESSOA
+as
+CREATE TYPE PERSON AS OBJECT (
+ NOME VARCHAR2(15),
+ SOBRENOME VARCHAR2(15)
+ 
+);
+  function INSERINOME(
+     NOME IN VARCHAR2,
+     SOBRENOME IN VARCHAR2
+    
+  ) return VARCHAR2;
+
+    PROCEDURE GETPESSOA(
+         SOBRENOME IN VARCHAR2
+    );
+
+   function constructormath(
+      NUMERO in NUMBER,
+      VALOR IN NUMBER
+   ) return PERSON;
+
+   
+end PESSOA;   
+
+CREATE OR REPLACE PACKAGE BODY math aS
+ FUNCTION INSERINOME(
+    numero number,
+    valor number
+ )return NUMBER 
+ IS
+ valorf number ;
+ BEGIN
+    return numero + valor ;
+    END;
+
+    PROCEDURE MULTIPLE(
+        NUMERO NUMBER,
+        VALOR NUMBER
+    )IS
+        
+    BEGIN
+    DBMS_OUTPUT.PUT_LINE(NUMERO * VALOR);
+    
+    END ;
+ END;
+
+
+
+
